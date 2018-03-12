de5b24a50a15ee196f29a5325fce6b189acf0d50
Diego Lopez Falcon <diego.lopez.falcon@gmail.com>
2018-03-12 11:26:46 -0700

    Style the errors and messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-  
+
   def index
     @articles = Article.all
   end
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was updated!"
+      flash[:success] = "Article was updated!"
       redirect_to article_path(@article)
     else
       render :edit
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was created!"
+      flash[:success] = "Article was created!"
       redirect_to article_path(@article)
     else
       render :new
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article was deleted!"
+    flash[:danger] = "Article was deleted!"
     redirect_to articles_path
   end
 
