f5042309d4451a690bf274375ebf3819838f845d
Diego Lopez Falcon <diego.lopez.falcon@gmail.com>
2018-03-12 16:02:42 -0700

    Add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   has_many :articles
   before_save {self.email = email.downcase}
   validates :username, presence: true, length: {minimum: 3, maximum: 25},
@@ -7,4 +8,6 @@ class User < ApplicationRecord
   validates :email, presence:true, length:{maximum: 105},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}
+
+  has_secure_password
 end
